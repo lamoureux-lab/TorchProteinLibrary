63c091e36653cbb96827edad5134220f8a6fa163
lupoglaz <georgy.derevyanko@gmail.com>
2017-12-21 17:06:58 -0500

    upd

--- PythonInterface/C_alpha_protein/__init__.py
+++ PythonInterface/C_alpha_protein/__init__.py
@@ -3,3 +3,6 @@ from Angles2CoordsDihedral import Angles2CoordsDihedral
 from Coords2Pairs import Coords2Pairs
 from Coords2RMSD import Coords2RMSD
 from Pairs2Distributions import Pairs2Distributions
+
+from Angles2CoordsAB import Angles2CoordsAB
+
--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
-from PythonInterface import CppLayers, Visualization, Utils, Angles2CoordsDihedral, Angles2BasisDihedral, Coords2Pairs, Coords2RMSD, Pairs2Distributions
+from PythonInterface import CppLayers, Visualization, Utils, Angles2CoordsDihedral, \
+Angles2BasisDihedral, Coords2Pairs, Coords2RMSD, Pairs2Distributions, Angles2CoordsAB
