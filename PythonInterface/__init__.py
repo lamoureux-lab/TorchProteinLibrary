067f9b4df4152b619777d462060c8d07e491ca61
lupoglaz <georgy.derevyanko@gmail.com>
2018-02-07 16:44:12 -0500

    Some init updates

--- PythonInterface/__init__.py
+++ PythonInterface/__init__.py
@@ -5,9 +5,9 @@
 # from C_alpha_protein import Angles2CoordsDihedral, Angles2BasisDihedral, Coords2Pairs, Coords2RMSDAB, Pairs2Distributions
 # from C_alpha_protein import Angles2CoordsAB
 
-from FullAtomModel import Angles2Coords, Angles2Coords_save
-from FullAtomModel import Coords2RMSD
-from FullAtomModel import cppPDB2Coords
+# from FullAtomModel import Angles2Coords, Angles2Coords_save
+# from FullAtomModel import Coords2RMSD
+# from FullAtomModel import cppPDB2Coords
 from FullAtomModel import PDB2Volume
 
-from Visualization import VisualizeVolume4d, visSequence, updateAngles
+# from Visualization import VisualizeVolume4d, visSequence, updateAngles
