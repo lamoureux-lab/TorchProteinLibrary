b53fa8b30173a683d3857805ecfec1669ed1b2fd
lupoglaz <georgy.derevyanko@gmail.com>
2019-10-04 12:39:57 -0400

    End tutorial

--- Layers/Physics/Coords2Eps/coords2eps_interface.cpp
+++ Layers/Physics/Coords2Eps/coords2eps_interface.cpp
@@ -1,16 +1,29 @@
 #include <torch/extension.h>
 #include "coords2eps_interface.h"
 #include <iostream>
+#include <math.h>
 
 torch::Tensor test(torch::Tensor coords, torch::Tensor num_atoms){
     torch::Tensor result = torch::zeros({30, 30, 30});
-    auto coords_a = coords.accessor<double, 2>();
-    auto num_atoms_a = num_atoms.accessor<int, 1>();
-    std::cout<<num_atoms[0]<<std::endl;
-    for(int i=0; i<num_atoms_a[0]; i++){
-        // std::cout<<coords_a[0][3*i + 0]<<", ";
-        // std::cout<<coords_a[0][3*i + 1]<<", ";
-        // std::cout<<coords_a[0][3*i + 2]<<"\n";
-    }
+    AT_DISPATCH_FLOATING_TYPES(coords.type(), "tmp", ([&] {
+        auto coords_a = coords.accessor<scalar_t, 2>();
+        auto num_atoms_a = num_atoms.accessor<int, 1>();
+        auto result_a = result.accessor<float, 3>();
+        std::cout<<num_atoms[0]<<std::endl;
+        
+        scalar_t resolution = 2.5;
+        for(int i=0; i<num_atoms_a[0]; i++){
+            scalar_t x = coords_a[0][3*i + 0];
+            scalar_t y = coords_a[0][3*i + 1];
+            scalar_t z = coords_a[0][3*i + 2];
+            int ix = floor(x/resolution);
+            int iy = floor(y/resolution);
+            int iz = floor(z/resolution);
+            if((ix<0)||(ix>=30)||(iy<0)||(iy>=30)||(iz<0)||(iz>=30))
+                continue;
+
+            result_a[ix][iy][iz] = 1.0;
+        }
+    }));
     return result;
 }   
--- tmp.py
+++ tmp.py
@@ -1,6 +1,21 @@
 import torch
 from TorchProteinLibrary.FullAtomModel import PDB2CoordsUnordered
+from TorchProteinLibrary.FullAtomModel import CoordsTranslate, Coords2Center
 import _Physics
+import _Volume
+
 p2c = PDB2CoordsUnordered()
+translate = CoordsTranslate()
+get_center = Coords2Center()
+
 coords, _, _, _, _, num_atoms = p2c(["tmp/1lzy.pdb"])
-print(_Physics.test(coords, num_atoms))
+
+center = get_center(coords, num_atoms)
+coords_ce = translate(coords, -center, num_atoms)
+box_center = torch.tensor([[30*2.5/2.0, 30*2.5/2.0, 30*2.5/2.0]], dtype=torch.double)
+coords_ce_ce = translate(coords_ce, box_center, num_atoms)
+
+coords_ce_ce = coords_ce_ce.to(dtype=torch.float32)
+res = _Physics.test(coords_ce_ce, num_atoms)
+print(res.sum())
+_Volume.Volume2Xplor(res, "tmp.xplor", 2.5)
