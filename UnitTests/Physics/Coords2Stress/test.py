dc42b33322c9ff4d197463728fa261d24854fb18
lupoglaz <georgy.derevyanko@gmail.com>
2019-10-20 20:31:48 -0400

    Stress tensor: work in progress

--- UnitTests/Physics/Coords2Stress/test.py
+++ UnitTests/Physics/Coords2Stress/test.py
@@ -5,7 +5,7 @@ import numpy as np
 import unittest
 from TorchProteinLibrary.FullAtomModel import PDB2CoordsUnordered
 from TorchProteinLibrary.FullAtomModel import CoordsTranslate, Coords2Center
-from TorchProteinLibrary.Physics import AtomNames2Params, ElectrostaticParameters, Coords2Elec
+from TorchProteinLibrary.Physics import AtomNames2Params, Coords2Stress
 import _Volume
 
 import prody as pd 
@@ -13,7 +13,9 @@ import prody as pd
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from AtomNames2Params import TestAtomNames2Params
 
-class TestCoords2Stress(TestAtomNames2Params):
+import matplotlib.pylab as plt
+
+class TestCoords2Stress(unittest.TestCase):
 	device = 'cpu'
 	dtype = torch.double
 	places = 7
@@ -25,24 +27,37 @@ class TestCoords2Stress(TestAtomNames2Params):
 	msg = "Testing stress tensor computation"
 
 	def setUp(self):
-		super(TestCoords2Stress, self).setUp()
+		num_at = 10
+		self.coords = torch.randn(1, num_at*3, dtype=torch.float, device=self.device)
+		self.num_atoms = torch.tensor([num_at], dtype=torch.int, device=self.device)
+		for i in range(num_at):
+			self.coords[0, 3*i + 0] = i*2.0 + 2.0
+			self.coords[0, 3*i + 1] = i*1.5 + 2.0
+			self.coords[0, 3*i + 2] = i*1.5 + 2.0
+
+		self.coords2stress = Coords2Stress()
+		
+		
 		
 		
 
 class TestCoords2Stress_forward(TestCoords2Stress):
 
 	def runTest(self):
-		pdb = pd.parsePDB('test.pdb')
-		atoms = pdb.select('protein')
-		gnm = pd.GNM('Test')
-		gnm.buildKirchhoff(atoms)
-		gnm.calcModes()
-		pd.showContactMap(gnm)
-		print(gnm.getVariances())
-		print(gnm.getCutoff())
-		print(gnm.getGamma())
-		print(gnm.getCovariance().round(2))
-		pass
+		# pdb = pd.parsePDB('test.pdb')
+		# atoms = pdb.select('protein')
+		# gnm = pd.GNM('Test')
+		# gnm.buildKirchhoff(atoms)
+		# gnm.calcModes()
+		# pd.showContactMap(gnm)
+		# print(gnm.getVariances())
+		# print(gnm.getCutoff())
+		# print(gnm.getGamma())
+		# print(gnm.getCovariance().round(2))
+		
+		dist_mat = self.coords2stress(self.coords, self.num_atoms)
+		plt.imshow(dist_mat[0,:,:].numpy())
+		plt.show()
 
 
 if __name__=='__main__':
