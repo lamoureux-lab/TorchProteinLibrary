905179ae3a92e584acd398ee180063a3672603be
truzmeto <truzmeto@kent.edu>
2019-10-22 17:37:18 -0400

    comparison tests

--- UnitTests/Physics/Coords2Elec/test.py
+++ UnitTests/Physics/Coords2Elec/test.py
@@ -7,87 +7,160 @@ from TorchProteinLibrary.FullAtomModel import PDB2CoordsUnordered
 from TorchProteinLibrary.FullAtomModel import CoordsTranslate, Coords2Center
 from TorchProteinLibrary.Physics import AtomNames2Params, ElectrostaticParameters, Coords2Elec
 import _Volume
-
+# reads .cube Delphi's output and converts to np array
+from read_cube import cube2numpy
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from AtomNames2Params import TestAtomNames2Params
 
-class TestCoords2Eps(TestAtomNames2Params):
-	device = 'cuda'
-	dtype = torch.float
-	msg = "Testing dielectric constant"
-
-	def setUp(self):
-		super(TestCoords2Eps, self).setUp()
-		self.translate = CoordsTranslate()
-		self.get_center = Coords2Center()
-		box_size = 60
-		res = 0.5
-		self.c2e = Coords2Elec(box_size=box_size, resolution=res)
-		self.box_center = torch.tensor([[res*box_size/2.0, res*box_size/2.0, res*box_size/2.0]], dtype=torch.double, device='cpu')
-
-class TestCoords2Eps_forward(TestCoords2Eps):
-
-	def runTest(self):
-		coords, chain_names, resnames, resnums, anames, num_atoms = self.p2c(["test.pdb"])
-		params = self.a2p(resnames, anames, num_atoms, self.elec_params.types, self.elec_params.params)
-		center = self.get_center(coords, num_atoms)
-		ce_coords = self.translate(coords, -center + self.box_center, num_atoms)
-		q, eps, phi = self.c2e(	ce_coords.to(device=self.device, dtype=self.dtype), 
-							params.to(device=self.device, dtype=self.dtype), 
-							num_atoms.to(device=self.device))
-		
-		_Volume.Volume2Xplor(phi[0,:,:,:].to(device='cpu'), "tmp.xplor", 1.0)
-
-		import pyvista as pv
-		p = pv.Plotter(point_smoothing=True)
-		q = torch.abs(q[0,:,:,:]).to(device='cpu').numpy()
-		phi = torch.abs(phi[0,:,:,:]).to(device='cpu').numpy()
-		# eps = eps[0,:,:,:].to(device='cpu').numpy()
-		p.add_volume(phi, cmap="viridis", opacity="linear")
-		# p.add_volume(eps, cmap="coolwarm", opacity="linear_r")
-		p.show()
+import pyvista as pv
+import matplotlib.pylab as plt
 
 
-class TestCoords2Eps_backward(TestCoords2Eps):
+class TestCoords2EpsSingleAtom(unittest.TestCase):
+    device = 'cuda'
+    dtype = torch.float
+    msg = "Testing dielectric constant on a single atom"
+    #delphiPath = "/home/lupoglaz/Programs/Delphi/Delphicpp_Linux/delphi_v77/bin/delphicpp_release"
+    delphiPath = "/home/talatnt/Projects/Research/Delphi/Delphicpp_v8.4.2_Linux/Release/delphicpp_release"
+    delphiParams = {
+        "input_files": {
+            "pdb": "born.pdb",
+            "siz": "born.siz",
+            "crg": "born.crg",
+        },
+        "output_files": {
+            "phi": "phimap.txt",
+            "eps": "epsmap.t",   # modified to avoid override
+        },
+        "float_params": {
+            "scale": 2.0,
+            "indi": 2.0,
+            "exdi": 80.0,
+            "prbrad": 0.0,
+            "ionrad": 0.0,
+            "salt": 0.0,
+            "sigma": 1.1,
+            "maxc": 0.0001
+        },
+        "int_params": {
+            "gsize": 61,
+            "bndcon": 1,
+            "nonit": 1,
+            "gaussian": 1,
+            "linit": 800,
+        }
+    }
 
-	def runTest(self):
-		pass
+    pdb_file = """ATOM      1  N   VAL D  10       0.000   0.000   0.000"""
+    siz_file = """atom__res_radius_
+        N     VAL   3.0          
+        """
+    crg_file = """atom__resnumbc_charge_
+        N     VAL      10.000            
+        """
+
+    def writeFiles(self, params):
+        with open(params["input_files"]["pdb"], 'w') as fout:
+            fout.write(self.pdb_file)
+        with open(params["input_files"]["siz"], 'w') as fout:
+            fout.write(self.siz_file)
+        with open(params["input_files"]["crg"], 'w') as fout:
+            fout.write(self.crg_file)
+
+    def runDelphi(self, params):
+        with open("params.txt", 'w') as fout:
+            for key in params["input_files"]:
+                fout.write('in(%s,file="%s")\n' %
+                           (key, params["input_files"][key]))
+            for key in params["output_files"]:
+                fout.write('out(%s,file="%s")\n' %   # modified in to out
+                           (key, params["output_files"][key]))
+            for key in params["float_params"]:
+                fout.write("%s=%f\n" % (key, params["float_params"][key]))
+            for key in params["int_params"]:
+                fout.write("%s=%d\n" % (key, params["int_params"][key]))
+
+        os.system(self.delphiPath + " params.txt")
+
+    def setUp(self):
+
+        self.translate = CoordsTranslate()
+        self.get_center = Coords2Center()
+        box_size = 60
+        res = 0.5
+
+        self.c2e = Coords2Elec(box_size=box_size, resolution=res)
+        self.p2c = PDB2CoordsUnordered()
+        self.get_center = Coords2Center()
+        self.elec_params = ElectrostaticParameters('single_atom', type='born')
+        self.a2p = AtomNames2Params()
+
+    def runTest(self):
+        self.writeFiles(params=self.delphiParams)
+        self.runDelphi(params=self.delphiParams)
+
+        Delphi, spatial_dim, res = cube2numpy(file_path="phimap.cube")
+        Eps, spatial_dim, res = cube2numpy(file_path="epsmap.txt")
+        EpsT, spatial_dim, res = cube2numpy(file_path="tal.cube")
+        
+        box_size = spatial_dim*res
+        box_center = torch.tensor(
+            [[box_size/2.0, box_size/2.0, box_size/2.0]], dtype=torch.double, device='cpu')
+
+        self.c2e = Coords2Elec(	box_size=spatial_dim,  # must be spatial_dim
+                                resolution=res,
+                                eps_in=2.0,
+                                eps_out=80.0,
+                                ion_size=0.0,
+                                wat_size=0.0,
+                                asigma=1.1,
+                                kappa02=0.0,
+                                charge_conv=7046.52,
+                                d=13)
+
+        prot = self.p2c(["single_atom/born.pdb"])
+        prot_center = self.get_center(prot[0], prot[-1])
+        coords_ce = self.translate(
+            prot[0], -prot_center + box_center, prot[-1])
+
+        params = self.a2p(prot[2], prot[4], prot[-1],
+                          self.elec_params.types, self.elec_params.params)
+        q, eps, phi = self.c2e(	coords_ce.to(device='cuda', dtype=torch.float),
+                                params.to(device='cuda', dtype=torch.float),
+                                prot[-1].to(device='cuda'))
+        #delphi_phi = torch.from_numpy(Delphi).clamp(0.0, 100.0).numpy()
+        delphi_phi = Delphi#.clip(0.0, 100.0)
+        this_eps = eps[0, 0, :, :, :].to(device='cpu').numpy()
+        #this_phi = phi[0, :, :, :].to(device='cpu').clamp(0.0, 100.0).numpy()
+        this_phi = phi[0, :, :, :].to(device='cpu')#.clamp(0.0, 100.0).numpy()
+
+        # p = pv.Plotter(point_smoothing=True)
+        # p.add_volume(Delphi, cmap="viridis", opacity="linear")
+        # p.add_volume(this_phi, cmap="viridis", opacity="linear")
+        # p.show()
+
+        f = plt.figure()
+        plt.subplot(121, title=r'$\phi$')
+        plt.plot(this_phi[:, int(spatial_dim/2),
+                          int(spatial_dim/2)], label='Our algorithm')
+        plt.plot(EpsT[:, int(spatial_dim/2),
+                          int(spatial_dim/2)], label='SOR algorithm')
+
+        
+        plt.plot(delphi_phi[:, int(spatial_dim/2),
+                            int(spatial_dim/2)], label='Delphi') 
+        
+        plt.legend(loc="best")
+
+        plt.subplot(122, title=r'$\epsilon$')
+        plt.plot(this_eps[:, int(spatial_dim/2),
+                          int(spatial_dim/2)], label='Our algorithm')
+        plt.plot(Eps[:, int(spatial_dim/2),
+                     int(spatial_dim/2)], label='Delphi')
+        plt.legend()
+        plt.show()
+
 
 if __name__ == '__main__':
-	unittest.main()
-
-	# # sphinx_gallery_thumbnail_number = 3
-	# import pyvista as pv
-	# import numpy as np
-	# from pyvista import examples
-	# # import vtk
-	
-	# # vtkgrid = vtk.vtkImageData()
-	# # print(vtkgrid)
-	# grid = pv.UniformGrid((10,10,10))
-	# grid.cell_arrays['init'] = np.random.rand(grid.n_cells)
-	# print(grid)
-	
-	# # Volume rendering is not supported with Panel yet
-	# pv.rcParams["use_panel"] = False
-
-	# bolt_nut = examples.download_bolt_nut()
-	# head = examples.download_head()
-	# print(bolt_nut.get(0).point_arrays.keys())
-	# print(bolt_nut.get(0).cell_arrays.keys())
-	# print(bolt_nut.get(0)['SLCImage'].dtype)
-	# print(head.point_arrays.keys())
-	# print(head.cell_arrays.keys())
-	
-	# vol = np.random.rand(1000).reshape((10,10,10))
-
-	# grid.plot(scalars=np.random.rand(grid.n_cells))
-	# p = pv.Plotter()
-	# p.add_volume(vol)
-	# p.show()
-
-	
-
-
-
+    unittest.main()
