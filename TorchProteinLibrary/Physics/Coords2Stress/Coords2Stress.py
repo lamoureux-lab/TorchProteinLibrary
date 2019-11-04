c6c9dc889916cb95f880ad22b89732a184e01626
lupoglaz <georgy.derevyanko@gmail.com>
2019-11-04 18:42:10 -0500

    Eigenvectors -> displacements. TODO: projection to volume, tests

--- TorchProteinLibrary/Physics/Coords2Stress/Coords2Stress.py
+++ TorchProteinLibrary/Physics/Coords2Stress/Coords2Stress.py
@@ -30,12 +30,16 @@ class Coords2StressFunction(Function):
 						
 		return None, None, None, None
 
-class Coords2Stress(Module):
+class Coords2Stress(nn.Module):
 	def __init__(self, box_size=80, resolution=1.0):
 		super(Coords2Stress, self).__init__()
 		self.sigma2 = 1.0
 			
 	def get_sep_mat(self, coords, num_atoms):
+		"""
+		Computes separation matrix: all coordinates minus all coordinates
+		"""
+
 		batch_size = coords.size(0)
 		max_num_atoms = int(coords.size(1)/3)
 		
@@ -46,42 +50,72 @@ class Coords2Stress(Module):
 			sep_mat = this_coords.unsqueeze(dim=1) - this_coords.unsqueeze(dim=0)
 			sep_mat_pad = F.pad(sep_mat, (0, 3*max_num_atoms - 3*num_at, 0, 3*max_num_atoms - 3*num_at), 'constant', 0.0)
 			sep_mats.append(sep_mat_pad)
+		
 		sep_mats = torch.stack(sep_mats, dim=0)
 		return sep_mats
 
-	def get_corr_mat(self, coords, num_atoms):
-		batch_size = coords.size(0)
-		max_num_atoms = int(coords.size(1)/3)
+	def get_hessian(self, sep_mat, num_atoms, cutoff = 7.0):
+		"""
+		Computes Hessian of the interaction potential. Interaction is cut off using gaussian
+		"""
 
-		corr_mats = []
-		for i in range(batch_size):
-			num_at = num_atoms[i].item()
-			this_coords = coords[i,:num_at*3].contiguous()
-			sep_mat = this_coords.unsqueeze(dim=1) - this_coords.unsqueeze(dim=0)
-			corr_mat_pad = F.pad(sep_mat, (0, 3*max_num_atoms - 3*num_at, 0, 3*max_num_atoms - 3*num_at), 'constant', 0.0)
-			corr_mats.append(corr_mat_pad)
-		corr_mats = torch.stack(corr_mats, dim=0)
-		return corr_mats
-	
-	def make_kirchoff(self, dist_mat, num_atoms):
-		batch_size = dist_mat.size(0)
-		max_num_atoms = int(dist_mat.size(1))
+		batch_size = sep_mat.size(0)
+		max_num_atoms = sep_mat.size(1)
+
+		dist_mat = torch.sqrt((sep_mat*sep_mat).sum(dim=3) + 1e-5)
+		dist_mat = dist_mat.unsqueeze(dim=3).unsqueeze(dim=4)
+		dist2_mat = dist_mat*dist_mat
+		hessian = (sep_mat.unsqueeze(dim=3)) * (sep_mat.unsqueeze(dim=4))
+		hessian = -hessian * torch.exp(-dist2_mat/(cutoff*cutoff)) / dist2_mat
+
 		for i in range(batch_size):
 			num_at = num_atoms[i].item()
 			for j in range(num_at):
-				dist_mat[i,j,j] = -(dist_mat[i, j, :j].sum() + dist_mat[i, j, j:].sum())
-		
-		return dist_mat
+				hessian[i,j,j,:,:] = -(hessian[i,j,:,:,:].sum(dim=0))
+
+		hessian = hessian.transpose(2,3).contiguous()	
+		return hessian.view(batch_size,max_num_atoms*3,max_num_atoms*3).contiguous()
+
+	def get_displacements(self, hessian, num_atoms):
+		"""
+		Computes larges eigenvector and returns 3*eigvec*eigval. This corresponds to
+		the displacements of atoms in [kBT/gamma] units
+		"""
+
+		batch_size = hessian.size(0)
+		max_num_coords = hessian.size(1)
+
+		eigvecs, eigvals = [], []
+		for i in range(batch_size):
+			num_coords = 3*num_atoms[i].item()
+			eigval, eigvec = torch.symeig(hessian[i,:num_coords, :num_coords], eigenvectors=True)
+			
+			if (torch.abs(eigval[-1]) > torch.abs(eigval[0])):
+				eigvec = eigvec[-1,:]
+				eigval = eigval[-1]
+			else:
+				eigvec = eigvec[0,:]
+				eigval = eigval[0]
+			
+			eigvals.append(eigval)
+			eigvec = F.pad(eigvec, (0, max_num_coords - num_coords), 'constant', 0.0)
+			eigvecs.append(eigvec)
+
+		eigvecs = torch.stack(eigvecs, dim=0)
+		eigvals = torch.stack(eigvals, dim=0)
+
+		return 3*eigvecs*eigvals
+
 
 	def forward(self, coords, num_atoms):
+		batch_size = coords.size(0)
+		max_coords = coords.size(1)
+
 		sep_mat = self.get_sep_mat(coords, num_atoms)
+		hessian = self.get_hessian(sep_mat, num_atoms)
+		displacements = self.get_displacements(hessian, num_atoms)
 		
-		# dist_mat = -torch.exp(-dist_mat/self.sigma2)
-		# kirchoff = self.make_kirchoff(dist_mat, num_atoms)
-
-
-
-		return kirchoff
+		return hessian, displacements
 
 		
 		
--- UnitTests/Physics/Coords2Stress/test.py
+++ UnitTests/Physics/Coords2Stress/test.py
@@ -5,7 +5,7 @@ import numpy as np
 import unittest
 from TorchProteinLibrary.FullAtomModel import PDB2CoordsUnordered
 from TorchProteinLibrary.FullAtomModel import CoordsTranslate, Coords2Center
-from TorchProteinLibrary.Physics import AtomNames2Params#, Coords2Stress
+from TorchProteinLibrary.Physics import AtomNames2Params, Coords2Stress
 import _Volume
 
 import prody as pd 
@@ -15,65 +15,6 @@ from AtomNames2Params import TestAtomNames2Params
 
 import matplotlib.pylab as plt
 
-from torch import nn
-import torch.nn.functional as F
-class Coords2Stress(nn.Module):
-	def __init__(self, box_size=80, resolution=1.0):
-		super(Coords2Stress, self).__init__()
-		self.sigma2 = 1.0
-			
-	def get_sep_mat(self, coords, num_atoms):
-		batch_size = coords.size(0)
-		max_num_atoms = int(coords.size(1)/3)
-		
-		sep_mats = []
-		for i in range(batch_size):
-			num_at = num_atoms[i].item()
-			this_coords = coords[i,:num_at*3].view(num_at, 3).contiguous()
-			sep_mat = this_coords.unsqueeze(dim=1) - this_coords.unsqueeze(dim=0)
-			sep_mat_pad = F.pad(sep_mat, (0, 3*max_num_atoms - 3*num_at, 0, 3*max_num_atoms - 3*num_at), 'constant', 0.0)
-			sep_mats.append(sep_mat_pad)
-		sep_mats = torch.stack(sep_mats, dim=0)
-		return sep_mats
-
-	def get_hessian(self, sep_mat, num_atoms):
-		batch_size = sep_mat.size(0)
-		max_num_atoms = sep_mat.size(1)
-
-		dist_mat = torch.sqrt((sep_mat*sep_mat).sum(dim=3) + 1e-5)
-		dist_mat = dist_mat.unsqueeze(dim=3).unsqueeze(dim=4)
-		dist2_mat = dist_mat*dist_mat#*dist_mat*dist_mat
-		hessian = (sep_mat.unsqueeze(dim=3)) * (sep_mat.unsqueeze(dim=4))
-		hessian = -hessian / dist2_mat
-
-		# hessian = torch.einsum("ijklm->ijjlm",hessian)
-
-		for i in range(batch_size):
-			num_at = num_atoms[i].item()
-			for j in range(num_at):
-				hessian[i,j,j,:,:] = -(hessian[i,j,:,:,:].sum(dim=0))
-
-		hessian = hessian.transpose(2,3).contiguous()	
-		return hessian.view(batch_size,max_num_atoms*3,max_num_atoms*3).contiguous()
-
-	def forward(self, coords, num_atoms):
-		batch_size = coords.size(0)
-		max_coords = coords.size(1)
-
-		sep_mat = self.get_sep_mat(coords, num_atoms)
-		hessian = self.get_hessian(sep_mat, num_atoms)
-		
-		hessian_inverse = []
-		for i in range(batch_size):
-			num_coords = 3*num_atoms[i].item()
-			Hinv = torch.pinverse(hessian[i,:num_coords,:num_coords])
-			F.pad(Hinv, (0, max_coords - num_coords, 0, max_coords - num_coords), 'constant', 0.0)
-			hessian_inverse.append(Hinv)
-
-		hessian_inverse = torch.stack(hessian_inverse, dim=0)
-		coords_displ = torch.einsum("ijj->ij",hessian_inverse)
-		
-		return hessian
 		
 
 class TestCoords2Stress(unittest.TestCase):
@@ -89,6 +30,7 @@ class TestCoords2Stress(unittest.TestCase):
 
 	def setUp(self):
 		num_at = 10
+		
 		self.coords = torch.randn(1, num_at*3, dtype=torch.float, device=self.device)
 		self.num_atoms = torch.tensor([num_at], dtype=torch.int, device=self.device)
 		for i in range(num_at):
@@ -97,28 +39,48 @@ class TestCoords2Stress(unittest.TestCase):
 			self.coords[0, 3*i + 2] = 0.0#i*1.5 + 2.0
 
 		self.coords2stress = Coords2Stress()
-		
-		
-		
-		
+
 
 class TestCoords2Stress_forward(TestCoords2Stress):
 
 	def runTest(self):
-		# pdb = pd.parsePDB('test.pdb')
-		# atoms = pdb.select('protein')
-		# gnm = pd.GNM('Test')
-		# gnm.buildKirchhoff(atoms)
-		# gnm.calcModes()
-		# pd.showContactMap(gnm)
-		# print(gnm.getVariances())
-		# print(gnm.getCutoff())
-		# print(gnm.getGamma())
-		# print(gnm.getCovariance().round(2))
+		pdb = pd.parsePDB('test.pdb')
+		atoms = pdb.select('protein')
+
+		self.coords = torch.randn(1, len(atoms)*3, dtype=torch.float, device=self.device)
+		self.num_atoms = torch.tensor([len(atoms)], dtype=torch.int, device=self.device)
 		
-		dist_mat = self.coords2stress(self.coords, self.num_atoms)
+		for i, atom in enumerate(atoms):
+			self.coords[0, i*3 + 0] = atom.getCoords()[0]
+			self.coords[0, i*3 + 1] = atom.getCoords()[1]
+			self.coords[0, i*3 + 2] = atom.getCoords()[2]
+
+		anm = pd.ANM('p38 ANM analysis')
+		anm.buildHessian(atoms)
+				
+		anm.calcModes(n_modes=1)
+		print(anm.getEigvals())
+		print(anm.getCovariance())
+		
+		dist_mat, dr = self.coords2stress(self.coords, self.num_atoms)
+		print(dr)
+		
+		
+		f = plt.figure(figsize=(15,5))
+		plt.subplot(1,3,1)
 		plt.imshow(dist_mat[0,:,:].numpy())
 		plt.colorbar()
+
+		plt.subplot(1,3,2)
+		plt.imshow(anm.getHessian())
+		plt.colorbar()
+
+		plt.subplot(1,3,3)
+		plt.imshow(dist_mat[0,:,:].numpy() - anm.getHessian())
+		plt.colorbar()
+
+		plt.tight_layout()
+		
 		plt.show()
 
 
