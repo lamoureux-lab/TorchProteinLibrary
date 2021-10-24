0816578d9e51f696f69fdf891fc15275eb4f3e1f
fmatreale <fmatreale93@gmail.com>
2021-10-24 11:25:54 -0400

    na: added cMatrix.py

---
+++ na/cMatrix.py
@@ -0,0 +1,29 @@
+import math as m
+import numpy as np
+
+## functions from cVector3.cpp
+##function to create a 3d Vector
+
+## functions from cMatrix33.cpp
+##function to create a 3x3 Matrix
+
+## functions from cMatrix.cpp
+##function to create a 4x4 Matrix
+
+##function to setDihedral
+
+##function to setDihedralDphi
+
+##function to setDihedralDpsi
+
+##function to setDihedralDr
+
+##function to setRx
+
+##function to setRy
+
+##function to setRz
+
+##function to setDRx
+
+##function to invertTransform44
