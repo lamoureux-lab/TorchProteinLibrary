18179b85491cb3c1e5091f4c99fb9e647e6397e9
anushriya <ansubedy@ucdavis.edu>
2021-10-29 16:23:31 -0400

    start of conformation.py

--- na/conformations.py
+++ na/conformations.py
@@ -1,7 +1,52 @@
 import math as m
 import numpy as np
+from cMatrix import *
 
-## functions from cConformation.cpp
-## update matrix
-## update updateDMatrix
 
+def updateMatrix(x, y,d, alpha, beta):
+    '''
+    :param x:
+    :param y:
+    :param d:
+    :param alpha:
+    :param beta:
+    :return:
+    '''
+    Ry = setRy(beta)
+    Rx = setRx(alpha)
+    mat = Ry * d * Rx
+
+def updateDMatrix(x, y,d, alpha, beta):
+    '''
+    :param x:
+    :param y:
+    :param d:
+    :param alpha:
+    :param beta:
+    :return: matrix for derivates wrt Rx
+    '''
+    Ry = setRy(beta)
+    DRx = setDRx(alpha)
+    mat = Ry * d * DRx
+
+def conformations(sequence, angle, angle_grad,  angles_length, atoms_global):
+    '''
+    :param sequence:
+    :param angle:
+    :param angle_grad:
+    :param angles_length:
+    :param atoms_global:
+    :return:
+    '''
+    for i in range(len(sequence)):
+        # T *phi = angles + i + angles_length*0;T *dphi = angles_grad + i + angles_length*0;
+        #T *psi = angles + i + angles_length*1;T *dpsi = angles_grad + i + angles_length*1;
+
+    ## cMatrix needs additional angles info
+        pass
+
+
+'''
+Function for Graphing the transformation of the backbones. 
+Represent each strand separately
+'''
