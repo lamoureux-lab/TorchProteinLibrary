2c3706cdfe1fcdc8df641a21a4abeda505d322f7
fmatreale <fmatreale93@gmail.com>
2022-09-12 10:49:57 -0400

    Updated pdb2coords_interface.cpp

--- Layers/FullAtomModel/PDB2Coords/pdb2coords_interface.cpp
+++ Layers/FullAtomModel/PDB2Coords/pdb2coords_interface.cpp
@@ -20,73 +20,73 @@ void PDB2CoordsOrdered( torch::Tensor filenames, torch::Tensor coords, torch::Te
         ERROR("Incorrect input ndim");
     }
     if (polymer_type == 0){
-        int batch_size = filenames.size(0);
-        std::string resLastAtom("OXT");
-
-        #pragma omp parallel for
-        for(int i=0; i<batch_size; i++){
-            torch::Tensor single_filename = filenames[i];
-            std::string filename = StringUtil::tensor2String(single_filename);
-
-            cPDBLoader pdb(filename, 0);
-            num_atoms[i] = 0;
-            int previous_res_num = pdb.res_nums[0];
-            for(int j=0; j<pdb.r.size(); j++){
-                if (previous_res_num < pdb.res_nums[j]) {
-                    previous_res_num = pdb.res_nums[j];
-                    num_atoms[i] += int(ProtUtil::getAtomIndex(pdb.res_names[j-1], resLastAtom));
-                }
-            }
-            num_atoms[i] += int(ProtUtil::getAtomIndex(pdb.res_names[pdb.r.size()-1], resLastAtom));
-        }
-
-        int max_num_atoms = num_atoms.max().data<int>()[0];
-        int64_t size_nums[] = {batch_size, max_num_atoms};
-        int64_t size_coords[] = {batch_size, max_num_atoms*3};
-        int64_t size_names[] = {batch_size, max_num_atoms, 4};
-
-        coords.resize_(torch::IntList(size_coords, 2)).fill_(0.0);
-        chain_names.resize_(torch::IntList(size_names, 3)).fill_(0);
-        res_names.resize_(torch::IntList(size_names, 3)).fill_(0);
-        res_nums.resize_(torch::IntList(size_nums, 2)).fill_(0);
-        atom_names.resize_(torch::IntList(size_names, 3)).fill_(0);
-        atom_mask.resize_(torch::IntList(size_nums, 2)).fill_(0);
-
-
-        #pragma omp parallel for
-        for(int i=0; i<batch_size; i++){
-            torch::Tensor single_coords = coords[i];
-            torch::Tensor single_filename = filenames[i];
-            torch::Tensor single_chain_names = chain_names[i];
-            torch::Tensor single_res_names = res_names[i];
-            torch::Tensor single_res_nums = res_nums[i];
-            torch::Tensor single_atom_names = atom_names[i];
-            torch::Tensor single_mask = atom_mask[i];
-
-            std::string filename = StringUtil::tensor2String(single_filename);
-
-            cPDBLoader pdb(filename, 0);
-
-            int global_ind = 0;
-            int previous_res_num = pdb.res_nums[0];
-            for(int j=0; j<pdb.r.size(); j++){
-                if (previous_res_num < pdb.res_nums[j]) {
-                    previous_res_num = pdb.res_nums[j];
-                    global_ind += ProtUtil::getAtomIndex(pdb.res_names[j-1], resLastAtom);
-                }
-                uint idx = ProtUtil::getAtomIndex(pdb.res_names[j], pdb.atom_names[j]) + global_ind;
-
-                StringUtil::string2Tensor(pdb.chain_names[j], single_chain_names[idx]);
-                StringUtil::string2Tensor(pdb.res_names[j], single_res_names[idx]);
-                StringUtil::string2Tensor(pdb.atom_names[j], single_atom_names[idx]);
-                single_res_nums[idx] = pdb.res_nums[j];
-
-                single_coords[3*idx + 0] = pdb.r[j].v[0];
-                single_coords[3*idx + 1] = pdb.r[j].v[1];
-                single_coords[3*idx + 2] = pdb.r[j].v[2];
-                single_mask[idx] = 1;
-            }
-        }
+//        int batch_size = filenames.size(0);
+//        std::string resLastAtom("OXT");
+//
+////        #pragma omp parallel for
+//        for(int i=0; i<batch_size; i++){
+//            torch::Tensor single_filename = filenames[i];
+//            std::string filename = StringUtil::tensor2String(single_filename);
+//
+//            cPDBLoader pdb(filename, 0);
+//            num_atoms[i] = 0;
+//            int previous_res_num = pdb.res_nums[0];
+//            for(int j=0; j<pdb.r.size(); j++){
+//                if (previous_res_num < pdb.res_nums[j]) {
+//                    previous_res_num = pdb.res_nums[j];
+//                    num_atoms[i] += int(ProtUtil::getAtomIndex(pdb.res_names[j-1], resLastAtom));
+//                }
+//            }
+//            num_atoms[i] += int(ProtUtil::getAtomIndex(pdb.res_names[pdb.r.size()-1], resLastAtom));
+//        }
+//
+//        int max_num_atoms = num_atoms.max().data<int>()[0];
+//        int64_t size_nums[] = {batch_size, max_num_atoms};
+//        int64_t size_coords[] = {batch_size, max_num_atoms*3};
+//        int64_t size_names[] = {batch_size, max_num_atoms, 4};
+//
+//        coords.resize_(torch::IntList(size_coords, 2)).fill_(0.0);
+//        chain_names.resize_(torch::IntList(size_names, 3)).fill_(0);
+//        res_names.resize_(torch::IntList(size_names, 3)).fill_(0);
+//        res_nums.resize_(torch::IntList(size_nums, 2)).fill_(0);
+//        atom_names.resize_(torch::IntList(size_names, 3)).fill_(0);
+//        atom_mask.resize_(torch::IntList(size_nums, 2)).fill_(0);
+//
+//
+////        #pragma omp parallel for
+//        for(int i=0; i<batch_size; i++){
+//            torch::Tensor single_coords = coords[i];
+//            torch::Tensor single_filename = filenames[i];
+//            torch::Tensor single_chain_names = chain_names[i];
+//            torch::Tensor single_res_names = res_names[i];
+//            torch::Tensor single_res_nums = res_nums[i];
+//            torch::Tensor single_atom_names = atom_names[i];
+//            torch::Tensor single_mask = atom_mask[i];
+//
+//            std::string filename = StringUtil::tensor2String(single_filename);
+//
+//            cPDBLoader pdb(filename, 0);
+//
+//            int global_ind = 0;
+//            int previous_res_num = pdb.res_nums[0];
+//            for(int j=0; j<pdb.r.size(); j++){
+//                if (previous_res_num < pdb.res_nums[j]) {
+//                    previous_res_num = pdb.res_nums[j];
+//                    global_ind += ProtUtil::getAtomIndex(pdb.res_names[j-1], resLastAtom);
+//                }
+//                uint idx = ProtUtil::getAtomIndex(pdb.res_names[j], pdb.atom_names[j]) + global_ind;
+//
+//                StringUtil::string2Tensor(pdb.chain_names[j], single_chain_names[idx]);
+//                StringUtil::string2Tensor(pdb.res_names[j], single_res_names[idx]);
+//                StringUtil::string2Tensor(pdb.atom_names[j], single_atom_names[idx]);
+//                single_res_nums[idx] = pdb.res_nums[j];
+//
+//                single_coords[3*idx + 0] = pdb.r[j].v[0];
+//                single_coords[3*idx + 1] = pdb.r[j].v[1];
+//                single_coords[3*idx + 2] = pdb.r[j].v[2];
+//                single_mask[idx] = 1;
+//            }
+//        }
     }
 
     else if (polymer_type == 1){
@@ -99,8 +99,8 @@ void PDB2CoordsOrdered( torch::Tensor filenames, torch::Tensor coords, torch::Te
 
             cPDBLoader pdb(filename, 1);
 //            std::cout << pdb.res_names << "cPDBLoader Test in pdb2coords \n"; //Test in pdb2coords of cPDBLoader 1
-            std::cout << res_names << "cPDBLoader Test in pdb2coords \n"; //Test in pdb2coords of cPDBLoader 2
-//            std::cout << "cPDBLoader Test in pdb2coords \n"; //Test in pdb2coords of cPDBLoader 3
+//            std::cout << res_names << "cPDBLoader Test in pdb2coords \n"; //Test in pdb2coords of cPDBLoader 2
+            std::cout << "cPDBLoader Test in pdb2coords \n"; //Test in pdb2coords of cPDBLoader 3
 //            num_atoms[i] = 0;
 //            int previous_res_num = pdb.res_nums[0];
 //            for(int j=0; j<pdb.r.size(); j++){
--- na/PDB2PDBna.py
+++ na/PDB2PDBna.py
@@ -18,34 +18,34 @@ import matplotlib.pylab as plt
 file = '/u2/home_u2/fam95/Documents/119d.pdb'
 
 # Functions used to Get Sequence
-def _convert2str(tensor):
-    return tensor.numpy().astype(dtype=np.uint8).tobytes().split(b'\00')[0]
-
-def get_sequence(res_names, res_nums, num_atoms, mask):
-    batch_size = res_names.size(0)
-    sequences = []
-    for batch_idx in range(batch_size):
-        sequence = ""
-        previous_resnum = res_nums[batch_idx, 0].item() - 1
-        for atom_idx in range(num_atoms[batch_idx].item()):
-            if mask[batch_idx, atom_idx].item() == 0: continue
-            if previous_resnum < res_nums[batch_idx, atom_idx].item():
-                residue_name = _convert2str(res_names[batch_idx, atom_idx, :]).decode("utf-8")
-                sequence = sequence + dindex_to_1[d3_to_index[residue_name]]
-                previous_resnum = res_nums[batch_idx, atom_idx].item()
-
-        residue_name = _convert2str(res_names[batch_idx, -1, :]).decode("utf-8")
-        sequence = sequence + dindex_to_1[d3_to_index[residue_name]]
-        sequences.append(sequence[:-1])
-    return sequences
+# def _convert2str(tensor):
+#     return tensor.numpy().astype(dtype=np.uint8).tobytes().split(b'\00')[0]
+#
+# def get_sequence(res_names, res_nums, num_atoms, mask):
+#     batch_size = res_names.size(0)
+#     sequences = []
+#     for batch_idx in range(batch_size):
+#         sequence = ""
+#         previous_resnum = res_nums[batch_idx, 0].item() - 1
+#         for atom_idx in range(num_atoms[batch_idx].item()):
+#             if mask[batch_idx, atom_idx].item() == 0: continue
+#             if previous_resnum < res_nums[batch_idx, atom_idx].item():
+#                 residue_name = _convert2str(res_names[batch_idx, atom_idx, :]).decode("utf-8")
+#                 sequence = sequence + dindex_to_1[d3_to_index[residue_name]]
+#                 previous_resnum = res_nums[batch_idx, atom_idx].item()
+#
+#         residue_name = _convert2str(res_names[batch_idx, -1, :]).decode("utf-8")
+#         sequence = sequence + dindex_to_1[d3_to_index[residue_name]]
+#         sequences.append(sequence[:-1])
+#     return sequences
 
 # PDB2Coords Function Called to Load Struct from File
 p2c = FullAtomModel.PDB2CoordsOrdered()
 loaded_prot = p2c([file], polymer_type=1)
 
 # Coords, Chains, Residue names and numbers, Atoms, and total number of atoms loaded from structure
-coords_dst, chainnames, resnames, resnums, atomnames, mask, num_atoms = loaded_prot
-coords_dst = coords_dst.to(dtype=torch.float)
+# coords_dst, chainnames, resnames, resnums, atomnames, mask, num_atoms = loaded_prot
+# coords_dst = coords_dst.to(dtype=torch.float)
 
 # sequences = get_sequence(resnames, resnums, num_atoms, mask)
 # print(sequences)
