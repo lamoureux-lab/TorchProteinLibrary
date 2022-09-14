d74a584db15d586382c705c7f9af8dd4b6fe5aa6
fmatreale <fmatreale93@gmail.com>
2022-09-14 10:45:24 -0400

    Updated PDBLoader

--- Layers/FullAtomModel/PDB2Coords/pdb2coords_interface.cpp
+++ Layers/FullAtomModel/PDB2Coords/pdb2coords_interface.cpp
@@ -41,8 +41,8 @@ void PDB2CoordsOrdered( torch::Tensor filenames, torch::Tensor coords, torch::Te
         }
 
         // Num_atoms Test
-        std::cout << "\n atoms names:" << pdb.atom_names << "\n res_names:" << pdb.res_names;
-        std::cout << "\n type atom names:" << typeid.atom_names;
+        std::cout << "\n atoms names:" << atom_names << "\n res_names:" << res_names;
+        std::cout << "\n type atom names:" << typeid(atom_names);
         std::cout << "\n num_atoms:" << num_atoms << "\n";
 
         int max_num_atoms = num_atoms.max().data<int>()[0];
