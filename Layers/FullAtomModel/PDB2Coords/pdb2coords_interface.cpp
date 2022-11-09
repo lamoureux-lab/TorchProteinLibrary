3f49736c2318440607a2e9aeaa69312dea218e64
fmatreale <fmatreale93@gmail.com>
2022-11-09 11:58:21 -0500

    pdb2Coords: update O5' index

--- Layers/FullAtomModel/PDB2Coords/pdb2coords_interface.cpp
+++ Layers/FullAtomModel/PDB2Coords/pdb2coords_interface.cpp
@@ -173,7 +173,7 @@ void PDB2CoordsOrdered( torch::Tensor filenames, torch::Tensor coords, torch::Te
 //            std::cout << pdb.chain_names[j];
 //            std::cout << pdb.res_nums[j];
                 if (pdb.chain_names[j] > chain_idx && pdb.atom_names[j] == "O5'"){
-                    chain_idx = pdb.chain_names[j]
+                    chain_idx = pdb.chain_names[j];
                     int res_idx = static_cast<int>(pdb.res_nums[j])
                     for pdb.res_nums[res_idx]{
                     int five_primeidx = 3;
