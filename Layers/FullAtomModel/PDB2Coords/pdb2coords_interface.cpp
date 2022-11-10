9a0f2e4ae9399f5fc354c1c10ab8a888f94ca113
fmatreale <fmatreale93@gmail.com>
2022-11-10 16:37:35 -0500

    pdb2Coords: update O5' index

--- Layers/FullAtomModel/PDB2Coords/pdb2coords_interface.cpp
+++ Layers/FullAtomModel/PDB2Coords/pdb2coords_interface.cpp
@@ -202,10 +202,10 @@ void PDB2CoordsOrdered( torch::Tensor filenames, torch::Tensor coords, torch::Te
                         single_mask[idx] = 1;
                         ++j;
 
-                        std::cout << "chain: " + chain_names[j] + "single: " + single_chain_names[idx] + "\n";
-                        std::cout << "res: " + res_names[j] + "single: " + single_res_names[idx] + "\n";
-                        std::cout << "atom: " + atom_names[j] + "single: " + single_atom_names[idx] + "\n";
-                        std::cout << "coords: " + pdb.r[j].v[:] + "single: " + single_coords[3 * idx] + single_coords[3 * idx +1]+ single_coords[3 * idx + 2]+ "\n";
+                        std::cout << "chain: " << chain_names[j] << "single: " << single_chain_names[idx] << "\n";
+                        std::cout << "res: " << res_names[j] << "single: " << single_res_names[idx] << "\n";
+                        std::cout << "atom: " << atom_names[j] << "single: " << single_atom_names[idx] << "\n";
+                        std::cout << "coords: " << pdb.r[j].v[:] << "single: " << single_coords[3 * idx] << single_coords[3 * idx +1] << single_coords[3 * idx + 2] << "\n";
                     }
                 }
                 if (previous_res_num < pdb.res_nums[j]) {
