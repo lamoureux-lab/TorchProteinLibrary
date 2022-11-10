3b2be2d2a679afc52593ee7ee6c085b65909038f
fmatreale <fmatreale93@gmail.com>
2022-11-10 18:22:01 -0500

    pdb2Coords: update O5' index

--- Layers/FullAtomModel/PDB2Coords/pdb2coords_interface.cpp
+++ Layers/FullAtomModel/PDB2Coords/pdb2coords_interface.cpp
@@ -176,7 +176,7 @@ void PDB2CoordsOrdered( torch::Tensor filenames, torch::Tensor coords, torch::Te
                     chain_idx = pdb.chain_names[j];
                     int res_idx = static_cast<int>(pdb.res_nums[j]);
                     while (pdb.res_nums[j] == pdb.res_nums[res_idx]){
-                        int five_primeidx = 4;
+                        int five_primeidx = 3;
 
                         if (previous_res_num < pdb.res_nums[j]) {
                             previous_res_num = pdb.res_nums[j];
@@ -201,10 +201,10 @@ void PDB2CoordsOrdered( torch::Tensor filenames, torch::Tensor coords, torch::Te
                         single_coords[3*idx + 2] = pdb.r[j].v[2];
                         single_mask[idx] = 1;
 
-                        std::cout << "chain: " << chain_names[j] << "single: " << single_chain_names[idx] << "\n";
-                        std::cout << "res: " << res_names[j] << "single: " << single_res_names[idx] << "\n";
-                        std::cout << "atom: " << atom_names[j] << "single: " << single_atom_names[idx] << "\n";
-                        std::cout << "coords: " << pdb.r[j].v[0] << pdb.r[j].v[1] << pdb.r[j].v[2] << "single: " << single_coords[3 * idx] << single_coords[3 * idx +1] << single_coords[3 * idx + 2] << "\n";
+                        std::cout << "chain: " << chain_names[j - five_primeidx] << "single: " << single_chain_names[idx] << "\n";
+                        std::cout << "res: " << res_names[j - five_primeidx] << "single: " << single_res_names[idx] << "\n";
+                        std::cout << "atom: " << atom_names[j - five_primeidx] << "single: " << single_atom_names[idx] << "\n";
+                        std::cout << "coords: " << pdb.r[j - five_primeidx].v[0] << pdb.r[j - five_primeidx].v[1] << pdb.r[j - five_primeidx].v[2] << "single: " << single_coords[3 * idx] << single_coords[3 * idx +1] << single_coords[3 * idx + 2] << "\n";
 
                         ++j;
                     }
