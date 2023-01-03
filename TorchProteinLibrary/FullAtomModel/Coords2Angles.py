028a450ae778fbbed5c006af394a554bd0a9c97d
fmatreale <fmatreale93@gmail.com>
2023-01-03 13:37:37 -0500

    Coords2Angles: update getBackbone

--- TorchProteinLibrary/FullAtomModel/Coords2Angles.py
+++ TorchProteinLibrary/FullAtomModel/Coords2Angles.py
@@ -143,6 +143,7 @@ def getBackbone(residues, polymer_type= 0):
 		chain_idx = str(0)
 
 		for i, res_i in enumerate(residues):
+			print(res_i)
 			if str(res_i.get_parent()) > chain_idx:  #& res_i.get_atom() == "O5'":
 				chain_idx = str(res_i.get_parent())
 				#res_idx = res_i
