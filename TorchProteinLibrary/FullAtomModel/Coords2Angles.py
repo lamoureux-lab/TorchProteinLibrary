826a557a17a598a2ef3b3c61f57cad61410383ca
fmatreale <fmatreale93@gmail.com>
2023-01-04 21:37:08 -0500

    Coords2Angles: update getBackbone

--- TorchProteinLibrary/FullAtomModel/Coords2Angles.py
+++ TorchProteinLibrary/FullAtomModel/Coords2Angles.py
@@ -232,6 +232,11 @@ def getBackbone(residues, polymer_type= 0):
 				if str(res_ip1.get_parent()) > chain_idx:
 					epsilon.append(0.0)
 					zeta.append(0.0)
+					nu0.append(calc_dihedral(C4_i, O4_i, C1_i, C2_i))
+					nu1.append(calc_dihedral(O4_i, C1_i, C2_i, C3_i))
+					nu2.append(calc_dihedral(C1_i, C2_i, C3_i, C4_i))
+					nu3.append(calc_dihedral(C2_i, C3_i, C4_i, O4_i))
+					nu4.append(calc_dihedral(C3_i, C4_i, O4_i, C1_i))
 					continue
 
 			if i < (len(residues) - 1):
