4c36152b23e3dfeed55c40fe43502bb154ad26ef
fmatreale <fmatreale93@gmail.com>
2023-01-21 16:54:09 -0500

    angles2coords_interface: modified save to test

--- Layers/FullAtomModel/Angles2Coords/angles2coords_interface.cpp
+++ Layers/FullAtomModel/Angles2Coords/angles2coords_interface.cpp
@@ -177,7 +177,8 @@ void Angles2Coords_save(    const char* sequence,
     }
     std::string aa(sequence);
     uint length = aa.length();
-    int num_atoms = ProtUtil::getNumAtoms(aa, add_terminal, int 1);
+    int polymer_type = 1;
+    int num_atoms = ProtUtil::getNumAtoms(aa, add_terminal, polymer_type);
     torch::Tensor dummy_grad = torch::zeros_like(input_angles);
     torch::Tensor dummy_coords = torch::zeros({3*num_atoms}, torch::TensorOptions().dtype(input_angles.dtype()));
     AT_DISPATCH_FLOATING_TYPES(input_angles.type(), "cConformation.save", ([&]{
@@ -191,6 +192,7 @@ void Angles2Coords_save(    const char* sequence,
 int getSeqNumAtoms( const char *sequence){
     bool add_terminal = false;
     std::string seq(sequence);
-    int num_atoms = ProtUtil::getNumAtoms(seq, add_terminal, int 1);
+    int polymer_type = 1;
+    int num_atoms = ProtUtil::getNumAtoms(seq, add_terminal, polymer_type);
     return num_atoms;
 }
