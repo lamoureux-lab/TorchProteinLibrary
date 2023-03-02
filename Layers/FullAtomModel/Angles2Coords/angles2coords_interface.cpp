7f3dd359d76752841ac95bceab840ffba5a3df2f
fmatreale <fmatreale93@gmail.com>
2023-03-02 15:17:29 -0500

    angles2coords_interface: modify num_atoms for test

--- Layers/FullAtomModel/Angles2Coords/angles2coords_interface.cpp
+++ Layers/FullAtomModel/Angles2Coords/angles2coords_interface.cpp
@@ -100,6 +100,7 @@ void Angles2Coords_forward(     torch::Tensor sequences,
 
                 int length = single_angles.sizes()[1];
 
+                std::cout << "res_names[i]" << res_names[i];
 //                int num_atoms = na_num_atoms;
                 int num_atoms = (res_names[i].size() * 6);
                 std::cout << "interface num_atoms" << num_atoms;
